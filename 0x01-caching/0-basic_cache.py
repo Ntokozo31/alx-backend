ca255309bb65d0b78aeecdf88ad6455d2eafbfed
Ntokozo31 <dubentokozo45@yahoo.com>
2024-11-06 16:15:28 +0000

    basic cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python3
 
+"""
+Basic cache dictionary
+"""
+
 
 class BaseCaching:
     def __init__(self):
         self.cache_data = {}
 
+
 class BasicCache(BaseCaching):
     def put(self, key, item):
         """Add an item to the cache."""
         if key is None or item is None:
-            return  # Do nothing if either key or item is None
+            return
         self.cache_data[key] = item
 
     def get(self, key):
         """Retrieve an item from the cache."""
         if key is None or key not in self.cache_data:
-            return None  # Return None if key is None or doesn't exist
+            return None
         return self.cache_data[key]
 
     def print_cache(self):
