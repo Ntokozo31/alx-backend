e60191ae108e168f0fe09a3b52ea866c0621f7f6
Ntokozo31 <dubentokozo45@yahoo.com>
2024-11-06 16:09:31 +0000

    basic-cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -2,21 +2,24 @@
 
 
 class BaseCaching:
-    """BaseCaching defines a caching system"""
     def __init__(self):
         self.cache_data = {}
 
-
 class BasicCache(BaseCaching):
-    """BaseCaching class that inherit from BaseCaching"""
-
     def put(self, key, item):
-        """Assign item into a dictionary self.cache_data with key"""
-        if key is not None and item is not None:
-            self.cache_data[key] = item
+        """Add an item to the cache."""
+        if key is None or item is None:
+            return  # Do nothing if either key or item is None
+        self.cache_data[key] = item
 
     def get(self, key):
-        """Return the value linked to key in self.cache_data"""
+        """Retrieve an item from the cache."""
         if key is None or key not in self.cache_data:
-            return None
+            return None  # Return None if key is None or doesn't exist
         return self.cache_data[key]
+
+    def print_cache(self):
+        """Print the current contents of the cache."""
+        print("Current cache:")
+        for key, value in self.cache_data.items():
+            print(f"{key}: {value}")
--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -11,33 +11,30 @@ class LIFOCache(BaseCaching):
 
     def __init__(self):
         """Initialize the LIFO cache and call parent init"""
-        super().__init__()  # Call the parent class's __init__ method
-        self.cache_data = OrderedDict()  # Use OrderedDict to maintain the order
+        super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """Assign item into self.cache_data with key"""
-        if key is not None and item is not None:  # Check for None
+       f key is not None and item is not None:
             if key in self.cache_data:
-                # If the key already exists, simply update it
                 self.cache_data[key] = item
                 return
 
             if len(self.cache_data) >= self.MAX_ITEMS:
-                # If cache is full, discard the last item (LIFO)
-                last_key, _ = self.cache_data.popitem()  # Pop the last item (LIFO)
-                print(f"DISCARD: {last_key}")  # Print discarded key
+                last_key, _ = self.cache_data.popitem()
+                print(f"DISCARD: {last_key}")
 
-            self.cache_data[key] = item  # Assign the item to the key
+            self.cache_data[key] = item
 
     def get(self, key):
         """Return the value linked to key in self.cache_data"""
-        if key is None or key not in self.cache_data:  # Check for None or non-existent key
+        if key is None or key not in self.cache_data:
             return None
-        return self.cache_data[key]  # Return the item
+        return self.cache_data[key]
 
     def print_cache(self):
         """Print the current cache data"""
         print("Current Cache Data:")
         for key, value in self.cache_data.items():
-            print(f"{key}: {value}")
-
+            print(f"{key}: {value}") 
