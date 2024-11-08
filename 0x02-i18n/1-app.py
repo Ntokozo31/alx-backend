49f64d0a78ff0ae425af40b4b5553a437795467e
Ntokozo31 <dubentokozo45@yahoo.com>
2024-11-08 19:16:40 +0000

    1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -13,6 +13,7 @@ app: Flask = Flask(__name__)
 
 
 class Config:
+    """Configaration class for babel"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
