8a37161ddfbba216188f4bf1213365a27d2f8eee
Ntokozo31 <dubentokozo45@yahoo.com>
2024-11-08 19:00:49 +0000

    1-app.py

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""
+This module setup basic babel
+"""
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app: Flask = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
