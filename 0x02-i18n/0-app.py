ec755968c8db873fb3c77a2d893dcbec164c6b18
Ntokozo31 <dubentokozo45@yahoo.com>
2024-11-08 19:20:15 +0000

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,15 +6,14 @@ This module randers basic flask app
 
 
 from flask import Flask, rander_tamplet
-from flask.typing import ResponseReturnValue
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index() -> ResponseReturnValue:
-    return rander_tamplat('0-index.html')
+def index() -> str:
+    return rander_tamplat("0-index.html")
 
 
 if __name__ == '__main__':
